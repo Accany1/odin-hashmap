08fef64df0d9e79911f564c326eee32bce8bad92
Accany1 <129660499+Accany1@users.noreply.github.com>
2024-11-09 16:42:32 +0800

    added all functionalities

--- javascript.js
+++ javascript.js
@@ -47,9 +47,20 @@ function HashMap() {
         } else {
             let pointer = hashmap[hashedKey][0]
             while (pointer.nextNode !== null) {
+                if (pointer.key === key ){
+                    pointer.value = value
+                    return
+                }
                 pointer = pointer.nextNode
             }
+
+            if (pointer.key === key ){
+                pointer.value = value
+                return
+            }
+
             pointer.nextNode = {key,value,nextNode}
+            // console.log(pointer)
             load ++
         }
 
@@ -86,6 +97,74 @@ function HashMap() {
         return null
     }
 
+    const has = (key) => {
+        for (i = 0; i<hashmap.length; i++) {
+            // if the array is not empty
+            if (hashmap[i].length !== 0){
+                // if this is the end of the linked list
+                if (hashmap[i][0].nextNode === null) {
+                    // if key in hash map = given key
+                    if (hashmap[i][0].key === key) {
+                        return true
+                    }
+                } else {
+                    let pointer = hashmap[i][0]
+                    while (pointer.nextNode !== null) {
+                        if (pointer.key === key) {
+                            return true
+                        }
+                        pointer = pointer.nextNode
+                    }
+                    if (pointer.key === key) {
+                        return true
+                    }
+                }
+            }
+        }
+        return false
+    }
+    
+    const remove = (key) => {
+        for (i = 0; i<hashmap.length; i++) {
+            // if the array is not empty
+            if (hashmap[i].length !== 0){
+                // if this is the end of the linked list
+                if (hashmap[i][0].nextNode === null) {
+                    // if key in hash map = given key
+                    if (hashmap[i][0].key === key) {
+                        hashmap[i] = []
+                        load --
+                        return true
+                    }
+                } else {
+                    let pointer = hashmap[i][0]
+                    let prevPointer = null
+                    while (pointer.nextNode !== null) {
+                        if (pointer.key === key) {
+                            let newPointer = pointer.nextNode
+                            pointer.key = newPointer.key
+                            pointer.value = newPointer.value
+                            pointer.nextNode = newPointer.nextNode
+                            load --
+                            return true
+                        }
+                        prevPointer = pointer
+                        pointer = pointer.nextNode
+                    }
+                    if (pointer.key === key) {
+                        prevPointer.nextNode = null
+                        load --
+                        return true
+                    }
+                }
+            }
+        }
+        return false
+    }
+
+    const length = () => {
+        return load
+    }
 
     const resize = () => {
         
@@ -118,11 +197,75 @@ function HashMap() {
         hashmap = new Array(capacity)
             .fill(null)
             .map(() => [])
+        load = 0
     }
 
+    const keys = () => {
+        let keyArray = []
+        for (i = 0; i<hashmap.length; i++) {
+            // if the array is not empty
+            if (hashmap[i].length !== 0){
+                // if this is the end of the linked list
+                if (hashmap[i][0].nextNode === null) {
+                    // if key in hash map = given key
+                        keyArray.push(hashmap[i][0].key)
+                    } else {
+                        let pointer = hashmap[i][0]
+                        while (pointer.nextNode !== null) {
+                            keyArray.push(pointer.key)
+                            pointer = pointer.nextNode
+                        }
+                        keyArray.push(pointer.key)
+                    }
+                } 
+            }
+        return keyArray
+    }
+
+    const values = () => {
+        let valueArray = []
+        for (i = 0; i<hashmap.length; i++) {
+            // if the array is not empty
+            if (hashmap[i].length !== 0){
+                // if this is the end of the linked list
+                if (hashmap[i][0].nextNode === null) {
+                    // if key in hash map = given key
+                        valueArray.push(hashmap[i][0].value)
+                    } else {
+                        let pointer = hashmap[i][0]
+                        while (pointer.nextNode !== null) {
+                            valueArray.push(pointer.value)
+                            pointer = pointer.nextNode
+                        }
+                        valueArray.push(pointer.value)
+                    }
+                } 
+            }
+        return valueArray
+    }
+        
+    
+
     const entries = () => {
-        console.log(hashmap)
-        console.log(hashmap.length)
+        let allArray = []
+        for (i = 0; i<hashmap.length; i++) {
+            // if the array is not empty
+            if (hashmap[i].length !== 0){
+                // if this is the end of the linked list
+                if (hashmap[i][0].nextNode === null) {
+                    // if key in hash map = given key
+                        allArray.push([hashmap[i][0].key, hashmap[i][0].value])
+                    } else {
+                        let pointer = hashmap[i][0]
+                        while (pointer.nextNode !== null) {
+                            allArray.push([pointer.key, pointer.value])
+                            pointer = pointer.nextNode
+                        }
+                        allArray.push([pointer.key, pointer.value])
+                    }
+                } 
+            }
+        return allArray
     }
 
     return {
@@ -130,11 +273,16 @@ function HashMap() {
         set,
         entries,
         clear,
-        get
+        get,
+        has,
+        remove,
+        length,
+        keys,
+        values
     }
 }
 
-const test = new HashMap() // or HashMap() if using a factory
+const test = new HashMap()
 
 test.set('apple', 'red')
 test.set('banana', 'yellow')
@@ -148,9 +296,18 @@ test.set('ice cream', 'white')
 test.set('jacket', 'blue')
 test.set('kite', 'pink')
 test.set('lion', 'golden')
-// test.set('lion', 'silver')
-// test.set('moon', 'silver')
+test.set('lion', 'silver')
+test.set('moon', 'silver')
 
-test.entries()
+// test.entries()
 console.log(test.get("lion"))
+console.log(test.has("lions"))
+console.log(test.remove("lion"))
+console.log(test.entries())
+console.log(test.length())
+console.log(test.keys())
+console.log(test.values())
+console.log(test.entries())
+
+
 
